34b197645577979a4c4a6267713e0a1de3c4d684
Scott <yeunghl@whu.edu.cn>
2018-10-13 15:55:23 +0800

    完善数据库表的设计 找了博客模板，开始修改

--- post/models.py
+++ post/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
+    id = models.IntegerField(primary_key=True,auto_created=True)
     title = models.CharField(max_length=40)
     type = models.CharField(max_length=20)
     content = models.TextField()
+    post_time = models.DateTimeField(auto_now=True)
+    see_count = models.IntegerField(default=0)
+    like = models.IntegerField(default=0)
+
 
     def __str__(self):
         return self.title
